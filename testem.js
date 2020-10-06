216dc1b7dde79e999e9dfbf73ea1c694018a4ef2
Pieter van Laatum <pieter.van.laatum@fabriquartz.com>
2020-10-06 19:47:02 +0200

    Upgrade Ember to version 3.21

--- testem.js
+++ testem.js
@@ -1,10 +1,17 @@
 /* eslint-disable camelcase */
+'use strict';
+
 module.exports = {
   test_page:        'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci:     ['Chrome'],
-  launch_in_dev:    ['Chrome'],
-  browser_args:     {
+  launch_in_ci:     [
+    'Chrome'
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_start_timeout: 120,
+  browser_args:          {
     Chrome: {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
